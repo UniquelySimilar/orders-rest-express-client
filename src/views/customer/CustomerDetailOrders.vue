a234e39bd32d0a7394a6e8c7554fe1c0c6425116
UniquelySimilar <consciousexplorer@gmail.com>
2020-12-11 09:38:29 -0700

    removed unnecessary date formatting and data type conversions

--- src/views/customer/CustomerDetailOrders.vue
+++ src/views/customer/CustomerDetailOrders.vue
@@ -1,67 +1,68 @@
 <template>
   <div id="customer-detail-orders">
-      <div>
-          <span class="component-heading">Customer Details</span>
-      </div>
-      <hr>
-      <div class="row">
+    <div>
+      <span class="component-heading">Customer Details</span>
+    </div>
+    <hr>
+    <div class="row">
+      <div class="col-md-4">
+        <div class="row">
           <div class="col-md-4">
-              <div class="row">
-                  <div class="col-md-4">
-                      <label>First Name:</label>
-                  </div>
-                  <div class="col-md-8">{{customer.first_name}}</div>
-              </div>
-              <div class="row">
-                  <div class="col-md-4">
-                      <label>Last Name:</label>
-                  </div>
-                  <div class="col-md-8">{{customer.last_name}}</div>
-              </div>
-              <div class="row">
-                  <div class="col-md-4">
-                      <label>Street:</label>
-                  </div>
-                  <div class="col-md-8">{{customer.street}}</div>
-              </div>
-              <div class="row">
-                  <div class="col-md-4">
-                      <label>City/State/Zip:</label>
-                  </div>
-                  <div class="col-md-8">{{getCityStateZip(customer)}}</div>
-              </div>
+            <label>First Name:</label>
           </div>
+          <div class="col-md-8">{{customer.first_name}}</div>
+        </div>
+        <div class="row">
           <div class="col-md-4">
-              <div class="row">
-                  <div class="col-md-4">
-                      <label>Home Phone:</label>
-                  </div>
-                  <div class="col-md-8">{{customer.home_phone}}</div>
-              </div>
-              <div class="row">
-                  <div class="col-md-4">
-                      <label>Work Phone:</label>
-                  </div>
-                  <div class="col-md-8">{{customer.work_phone}}</div>
-              </div>
-              <div class="row">
-                  <div class="col-md-4">
-                      <label>Email:</label>
-                  </div>
-                  <div class="col-md-8">{{customer.email}}</div>
-              </div>
-              <div class="row">
-                  <div class="col-md-4">
-                      <label>&nbsp;</label>
-                  </div>
-                  <div class="col-md-8">&nbsp;</div>
-              </div>
+            <label>Last Name:</label>
           </div>
+          <div class="col-md-8">{{customer.last_name}}</div>
+        </div>
+        <div class="row">
+          <div class="col-md-4">
+            <label>Street:</label>
+          </div>
+          <div class="col-md-8">{{customer.street}}</div>
+        </div>
+        <div class="row">
+          <div class="col-md-4">
+            <label>City/State/Zip:</label>
+          </div>
+          <div class="col-md-8">{{getCityStateZip(customer)}}</div>
+        </div>
       </div>
-      <hr>
-      <order-index v-if="customer.id" v-bind:customerId="customer.id" v-bind:initialOrders="customer.orders"></order-index>
-      <hr>
-      <router-link class="btn btn-outline-dark" :to="{ name: 'customerIndex' }">Back</router-link>
+      <div class="col-md-4">
+        <div class="row">
+          <div class="col-md-4">
+            <label>Home Phone:</label>
+          </div>
+          <div class="col-md-8">{{customer.home_phone}}</div>
+        </div>
+        <div class="row">
+          <div class="col-md-4">
+            <label>Work Phone:</label>
+          </div>
+          <div class="col-md-8">{{customer.work_phone}}</div>
+        </div>
+        <div class="row">
+          <div class="col-md-4">
+            <label>Email:</label>
+          </div>
+          <div class="col-md-8">{{customer.email}}</div>
+        </div>
+        <div class="row">
+          <div class="col-md-4">
+            <label>&nbsp;</label>
+          </div>
+          <div class="col-md-8">&nbsp;</div>
+        </div>
+      </div>
+    </div>
+    <hr>
+    <order-index v-if="customer.id" v-bind:customerId="customer.id" v-bind:initialOrders="customer.orders">
+    </order-index>
+    <hr>
+    <router-link class="btn btn-outline-dark" :to="{ name: 'customerIndex' }">Back</router-link>
   </div>
 </template>
 
@@ -70,54 +71,55 @@
   import { axios, processAjaxAuthError } from '../../global-vars.js'
 
   export default {
-      name: "CustomerDetailOrders",
-      components: {
-          OrderIndex
-      },
-      props: {
-          customerId: {
-              type: Number,
-              required: true
-          }
-      },
-      data() {
-          return {
-              customer: {
-                  id: 0,
-                  orders: []
-              },
-          }
-      },
-      computed: {
-          token() {
-              return this.$store.state.token
-          }
-      },
-      methods: {
-          getCityStateZip(customer) {
-              return customer.city + ", " + customer.state + " " + customer.zipcode;
-          }
-      },
-      // Lifecycle hooks
-      created() {
-        axios.get('/customers/' + this.customerId, {
-            headers: {
-                'Authorization': 'Bearer ' + this.token
-            }
-        })
+    name: "CustomerDetailOrders",
+    components: {
+      OrderIndex
+    },
+    props: {
+      customerId: {
+        type: Number,
+        required: true
+      }
+    },
+    data() {
+      return {
+        customer: {
+          id: 0,
+          orders: []
+        },
+      }
+    },
+    computed: {
+      token() {
+        return this.$store.state.token
+      }
+    },
+    methods: {
+      getCityStateZip(customer) {
+        return customer.city + ", " + customer.state + " " + customer.zipcode;
+      }
+    },
+    // Lifecycle hooks
+    created() {
+      axios.get('/customers/' + this.customerId, {
+        headers: {
+          'Authorization': 'Bearer ' + this.token
+        }
+      })
         .then(response => {
-            //console.log(response.data);
-            this.customer = response.data;
-            this.customer.id = parseInt(this.customer.id);
-            this.customer.orders.forEach(order => {
-                order.id = parseInt(order.id);
-                //console.log(order);
-            });
+          this.customer = response.data;
+          this.customer.orders.forEach(order => {
+            order.order_date = order.order_date.substring(0, 10);
+            order.required_date = order.required_date.substring(0, 10);
+            if (order.shipped_date) {
+              order.shipped_date = order.shipped_date.substring(0, 10);
+            }
+          });
         })
         .catch(error => {
-            processAjaxAuthError(error, this.$router);
+          processAjaxAuthError(error, this.$router);
         });
-      }
+    }
   }
 </script>
 
--- src/views/order/OrderIndex.vue
+++ src/views/order/OrderIndex.vue
@@ -1,44 +1,47 @@
 <template>
   <div id="order-index">
-      <div class="table-title">
-          <span class="component-heading">Orders</span>
-          <router-link class="btn btn-outline-dark" v-if="customerId" :to="{ name: 'orderCreate', params: { customerId: customerId } }">Create Order</router-link>
-      </div>
-      <div class="row" v-if="orders.length">
-          <div class="col-md-8">
-              <table class="table table-striped table-bordered">
-                  <thead>
-                      <tr>
-                          <th>Status</th>
-                          <th>Order Date</th>
-                          <th>Required Date</th>
-                          <th>Shipped Date</th>
-                          <th>&nbsp;</th>
-                          <th>&nbsp;</th>
-                          <th>&nbsp;</th>
-                      </tr>
-                  </thead>
-                  <!-- TODO: formatDate should no longer be necessary -->
-                  <tbody>
-                      <tr v-for="order in orders" v-bind:key="order.id">
-                          <td>{{ getOrderStatusStr(order.order_status) }}</td>
-                          <td>{{ formatDate(order.order_date) }}</td>
-                          <td>{{ formatDate(order.required_date) }}</td>
-                          <td>{{ formatDate(order.shipped_date) }}</td>
-                          <td><router-link :to="{ name: 'orderDetailLineItems', params: { orderId: order.id } }">Line Items</router-link></td>
-                          <td><router-link :to="{ name: 'orderEdit', params: { customerId: customerId, orderId: order.id } }">Edit</router-link></td>
-                          <td><a href="#" v-on:click="openDeleteModal(order.id)">Delete</a></td>
-                      </tr>
-                  </tbody>
-              </table>
+    <div class="table-title">
+      <span class="component-heading">Orders</span>
+      <router-link class="btn btn-outline-dark" v-if="customerId"
+        :to="{ name: 'orderCreate', params: { customerId: customerId } }">Create Order</router-link>
+    </div>
+    <div class="row" v-if="orders.length">
+      <div class="col-md-8">
+        <table class="table table-striped table-bordered">
+          <thead>
+            <tr>
+              <th>Status</th>
+              <th>Order Date</th>
+              <th>Required Date</th>
+              <th>Shipped Date</th>
+              <th>&nbsp;</th>
+              <th>&nbsp;</th>
+              <th>&nbsp;</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="order in orders" v-bind:key="order.id">
+              <td>{{ getOrderStatusStr(order.order_status) }}</td>
+              <td>{{ order.order_date }}</td>
+              <td>{{ order.required_date }}</td>
+              <td>{{ order.shipped_date }}</td>
+              <td>
+                <router-link :to="{ name: 'orderDetailLineItems', params: { orderId: order.id } }">Line
+                  Items</router-link>
+              </td>
+              <td>
+                <router-link :to="{ name: 'orderEdit', params: { customerId: customerId, orderId: order.id } }">
+                  Edit</router-link>
+              </td>
+              <td><a href="#" v-on:click="openDeleteModal(order.id)">Delete</a></td>
+            </tr>
+          </tbody>
+        </table>
 
-              <delete-modal
-              v-if="deleteModalOpen"
-              deleteMessage="Delete this order?"
-              @closeDeleteModalEvent="closeDeleteModal"
-              @deleteRecordEvent="deleteOrder" />
-              </div>
+        <delete-modal v-if="deleteModalOpen" deleteMessage="Delete this order?"
+          @closeDeleteModalEvent="closeDeleteModal" @deleteRecordEvent="deleteOrder" />
       </div>
+    </div>
   </div>
 </template>
 
@@ -47,70 +50,63 @@
   import DeleteModal from '../../components/DeleteModal.vue'
 
   export default {
-      name: 'OrderIndex',
-      components: {
-          DeleteModal
+    name: 'OrderIndex',
+    components: {
+      DeleteModal
+    },
+    props: {
+      customerId: {
+        type: Number,
+        required: true
       },
-      props: {
-          customerId: {
-              type: Number,
-              required: true
-          },
-          initialOrders: {
-              type: Array,
-              required: true
-          }
-      },
-      data() {
-          return {
-              orders: this.initialOrders,
-              deleteOrderId: 0,
-              deleteModalOpen: false
-          }
-      },
-      computed: {
-          token() {
-              return this.$store.state.token;
-          }
-      },
-      methods: {
-          formatDate(milliseconds) {
-              if (!milliseconds)
-                  return;
-
-              let date = new Date(milliseconds);
-              return (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear();
-          },
-          openDeleteModal(id) {
-              this.deleteOrderId = id;
-              this.deleteModalOpen = true;
-          },
-          closeDeleteModal() {
-              this.deleteModalOpen = false;
-          },
-          deleteOrder() {
-              let id = this.deleteOrderId;
-              axios.delete('/orders/' + id, {
-                  headers: {
-                      'Authorization': 'Bearer ' + this.token
-                  }
-              })
-              .then( () => {
-                  // Remove order from orders array
-                  this.orders = this.orders.filter( order => {
-                      return order.id !== id;
-                  });
-              })
-              .catch(error => {
-                  processAjaxAuthError(error, this.$router);
-              })
-              .finally( () => this.closeDeleteModal() );
-          },
-          getOrderStatusStr(status) {
-              // Wrap imported function
-              return getOrderStatusStr(status);
-          }
+      initialOrders: {
+        type: Array,
+        required: true
       }
+    },
+    data() {
+      return {
+        orders: this.initialOrders,
+        deleteOrderId: 0,
+        deleteModalOpen: false
+      }
+    },
+    computed: {
+      token() {
+        return this.$store.state.token;
+      }
+    },
+    methods: {
+      openDeleteModal(id) {
+        this.deleteOrderId = id;
+        this.deleteModalOpen = true;
+      },
+      closeDeleteModal() {
+        this.deleteModalOpen = false;
+      },
+      deleteOrder() {
+        let id = this.deleteOrderId;
+        axios.delete('/orders/' + id, {
+          headers: {
+            'Authorization': 'Bearer ' + this.token
+          }
+        })
+          .then(() => {
+            // Remove order from orders array
+            this.orders = this.orders.filter(order => {
+              return order.id !== id;
+            });
+          })
+          .catch(error => {
+            processAjaxAuthError(error, this.$router);
+          })
+          .finally(() => this.closeDeleteModal());
+      },
+      getOrderStatusStr(status) {
+        // Wrap imported function
+        return getOrderStatusStr(status);
+      }
+    }
   }
 
 </script>
